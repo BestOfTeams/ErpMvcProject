74edea7d6b30c7d975c669615a4c79c617479590
enessenoglu <enessenoglu954@gmail.com>
2021-12-16 00:14:03 +0300

    Current

---
+++ ErpMvcProject.Entity/CurrentGroups.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpMvcProject.Entity
+{
+public class CurrentGroups
+    {
+        public int Id { get; set; }
+        public string gName { get; set; }
+        public bool isActive { get; set; }
+        
+    }
+}
